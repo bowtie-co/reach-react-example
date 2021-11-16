8bf7833e9201fd5afbe3677f2e1567b8a7e5dba8
Charlie McClung <charlie@bowtie.co>
2021-11-15 17:13:49 -0700

    Initial commit

---
+++ src/lib/language/en.js
@@ -0,0 +1,23 @@
+export const en = {
+  general: {
+    new: 'New',
+    done: 'Done',
+    open: 'Open',
+    save: 'Save',
+    edit: 'Edit',
+    close: 'Close',
+    search: 'Search',
+    cancel: 'Cancel',
+    date_format: 'MM/DD/YYYY',
+    type: 'Type',
+    home: 'Home',
+    welcome: 'Welcome',
+    not_found: 'Not Found',
+    oops: 'Oops'
+  },
+  footer: {
+    powered_by: 'Powered by',
+    support_msg: 'For support message',
+    copyright: 'Copyright'
+  }
+};
---
+++ src/lib/language/es.js
@@ -0,0 +1,23 @@
+export const es = {
+  general: {
+    new: 'Nuevo',
+    done: 'Listo',
+    open: 'Abrir',
+    save: 'Guardar',
+    edit: 'Editar',
+    close: 'Cerrar',
+    search: 'Buscar',
+    cancel: 'Cancelar',
+    date_format: 'DD/MM/AAAA',
+    type: 'Tipo',
+    home: 'Hogar',
+    welcome: 'Bienvenidas',
+    not_found: 'No Encontrada',
+    oops: 'Uy'
+  },
+  footer: {
+    powered_by: 'Propulsado por',
+    support_msg: 'Para ayuda contacta a',
+    copyright: 'Derechos de autor'
+  }
+};
